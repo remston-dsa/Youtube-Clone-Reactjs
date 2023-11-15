ab841fed25c23cb9a210cd8fdcce1986aa2299d1
Remston Dsa <rd2995@nyu.edu>
2023-11-14 21:08:06 -0500

    copy right

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -27,7 +27,7 @@ const Feed = () => {
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff" }}>
-          Copyright © 2022 JSM Media
+          Copyright © 2023 Remston Dsa
         </Typography>
       </Box>
 
